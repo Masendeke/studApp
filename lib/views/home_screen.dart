615503630a7affe57d65c1d82a593be837b6d11f
Ndile0503 <mahlangup557@gmail.com>
2026-05-08 14:10:42 +0200

    Edit home screen
    added welcome card to home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,13 +2,44 @@ import 'package:flutter/material.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      appBar: AppBar(title: Text('Home')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+
+                  Text(
+                    'Manage your applications easily.',
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      
     );
   }
 }
