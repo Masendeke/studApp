c2f667414a856b19f2656470b82081a3e314f021
LonwaboTsh <Lonwabotshabane@gmail.com>
2026-05-09 20:14:07 +0200

    added status to models

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -8,6 +8,7 @@ class StudentApplication{
   String module2;
   String course;
 
+  String status;
 
 StudentApplication({
 required this.stdNo,
@@ -18,12 +19,14 @@ required this.yearOfStudy,
 required this.module1,
 required this.module2,
 required this.course,
+this.status = 'Pending', // This is the default status for newly created students
 
 
 });
  Map<String,dynamic> toJson()=>{
     'std_no':stdNo,'name':name,'surname':surname,'email':email,
     'course':course,'year':yearOfStudy,'module1':module1,'module2':module2,
+    'status': status,
   };
 
 }
